cebf4d34fd0fc5a1b0d2d05b8acecec83da17446
Daniel Schott <daniel.schott@student.reutlingen-university.de>
2020-05-31 16:54:08 +0200

    design changes in css

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -9,8 +9,8 @@ function start_socket_listeners(){
   event.preventDefault();
 
   var text_to_translate = $('#input_text_to_translate').val();
-  var language_to_translate_to = $('#input_language_to_translate_to').val();
-
+  var language_to_translate_to = $('#select_language_code').find(":selected").val();
+  console.log("lanugage code", language_to_translate_to);
     // create json object
   params = {
     text:text_to_translate,
@@ -28,7 +28,7 @@ function start_socket_listeners(){
 
   socket.on('text_translated', function(translation_result){
     console.log(translation_result);
-  
+
     $('#p_translated_text').text(translation_result.translations[0].translation);
     $('#p_identified_language').text(translation_result.detect_language);
     $('#p_confidence').text(translation_result.confidence);
